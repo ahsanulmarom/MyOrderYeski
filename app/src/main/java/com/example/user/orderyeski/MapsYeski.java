db48eb16f7c7eb5f8ea75a2da962d0fd305a8f89
Ahsanul Marom <ahsanulmarom@gmail.com>
2016-11-25 14:34:15 +0700

    Create MapsYeski.java

---
+++ app/src/main/java/com/example/user/orderyeski/MapsYeski.java
@@ -0,0 +1,17 @@
+package com.example.user.orderyeski;
+
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsYeski extends FragmentActivity implements OnMapReadyCallback {
+
+    //Isi Maps
+    
+}

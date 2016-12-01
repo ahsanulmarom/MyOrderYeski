577c3c8f071c01940f6262e2db679be6f2f78820
Ahsanul Marom <ahsanulmarom@gmail.com>
2016-12-01 12:44:50 +0700

    Update PromoPage.java

--- app/src/main/java/com/example/user/orderyeski/PromoPage.java
+++ app/src/main/java/com/example/user/orderyeski/PromoPage.java
@@ -15,8 +15,34 @@ import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
+    public void search_db(View v) {
+        String edit_db = this.et_db.getText().toString();
+        if(!edit_db.equals("")) {
+            try {
+                this.cursor = this.db.rawQuery("SELECT * FROM Promo WHERE nama LIKE ?", new String[]{"%" + edit_db + "%"});
+                this.adapter = new SimpleCursorAdapter(this, R.layout.promolayout, this.cursor, new String[]{"nama", "periode", "img"}, new int[]{R.id.tv_nama, R.id.tvPeriode, R.id.imV});
+                if(this.adapter.getCount() == 0) {
+                    Toast.makeText(this, "Tidak ditemukan data dengan kata kunci " + edit_db, 0).show();
+                } else {
+                    this.lv.setAdapter(this.adapter);
+                }
+            } catch (Exception var5) {
+                var5.printStackTrace();
+            }
+        } else {
+            try {
+                this.cursor = this.db.rawQuery("SELECT * FROM Promo ORDER BY nama ASC", (String[])null);
+                this.adapter = new SimpleCursorAdapter(this, R.layout.promolayout, this.cursor, new String[]{"nama", "periode", "img"}, new int[]{R.id.tv_nama, R.id.tvPeriode, R.id.imV});
+                this.lv.setAdapter(this.adapter);
+                this.lv.setTextFilterEnabled(true);
+            } catch (Exception var4) {
+                var4.printStackTrace();
+            }
+        }
 
-public void detail(int position) {
+    }
+
+    public void detail(int position) {
         int im = 0;
         String _id = "";
         String nama = "";
@@ -32,5 +58,5 @@ public void detail(int position) {
             syarat = this.cursor.getString(this.cursor.getColumnIndex("syarat"));
             periode = this.cursor.getString(this.cursor.getColumnIndex("periode"));
             de = this.cursor.getString(this.cursor.getColumnIndex("de"));
-        } //continue
-	}
+        }
+    }
